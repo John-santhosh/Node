f54c313a21e3e7decde87522aa23f94ac90d248e
John-santhosh <johnsantosh2@gmail.com>
2023-09-13 23:06:50 +0530

    node very basics day 1

---
+++ 1.server/math.js
@@ -0,0 +1,14 @@
+// export in node ##common way
+// const add = (a, b) => a + b;
+// const sub = (a, b) => a - b;
+// const div = (a, b) => a / b;
+// const mul = (a, b) => a * b;
+
+// module.exports = { add, sub, div, mul };
+
+// alternative way
+
+exports.add = (a, b) => a + b;
+exports.sub = (a, b) => a - b;
+exports.div = (a, b) => a / b;
+exports.mul = (a, b) => a * b;
---
+++ 1.server/server.js
@@ -0,0 +1,36 @@
+// 1) node runs on server - not in the browser
+// 2) the console is the terminal (cmd) window
+console.log("hello World");
+
+//3) global object instead of window object
+// global obj in node is much smaller than the window obj.
+// console.log(global);
+
+// 4) has common core modules
+//5) commonjs modules instead of ES6
+
+// ### Node js is missing js API's like fetch though we can use external packages to make it available.  ###
+
+const os = require("os");
+const path = require("path");
+
+// destructuring math
+const { add, sub, mul, div } = require("./math");
+
+console.log(add(5, 3));
+
+console.log(os.type());
+console.log(os.version());
+console.log(os.homedir());
+
+console.log(__dirname);
+console.log(__filename);
+
+// gives the same result as __dirname (gives the directory structure of the file location)
+console.log(path.dirname(__filename));
+// gives only the file name with extension
+console.log(path.basename(__filename));
+//gives only the extension of the current file
+console.log(path.extname(__filename));
+// gives every info about the file in an object which containing information about the file.
+console.log(path.parse(__filename));
