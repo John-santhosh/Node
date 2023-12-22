3d09da0ce6518a7f2cd564df9c0f3e6145acd162
John-santhosh <johnsantosh2@gmail.com>
2023-12-22 15:14:39 +0530

    node recap

--- 06_Express_JS/server.js
+++ 06_Express_JS/server.js
@@ -3,6 +3,10 @@ const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 3500;
 
+// check like a water fall from top '/' to bottom '/*'
+
+//app.get('^/$|index(.html)?'. (req, res) => {
+// supports regEx like above.
 app.get("/", (req, res) => {
   //  res.sendFile("./views/index.html", {
   //    root: __dirname,
@@ -10,22 +14,26 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
-app.get("/new-page.html", (req, res) => {
+// making .html optional and telling the server to respond with or without .html extension.
+app.get("/new-page(.html)?", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "new-page.html"));
 });
 
-app.get("/old-page.html", (req, res) => {
+app.get("/old-page(.html)?", (req, res) => {
+  // 301 permanently moved
   res.redirect(301, "/new-page.html"); // 302 by default
 });
 
 // Route handlers
 app.get(
-  "/hello.html",
+  "/hello(.html)?",
   (req, res, next) => {
     console.log("attempt to load hello ");
+    // we can chain function using next().
     next();
   },
   (req, res) => {
+    // the last one doesn't have a next() function
     res.send("hello world!");
   }
 );
@@ -51,7 +59,8 @@ app.get("/chain.html", [one, two, three]);
 // ^^
 
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "404.html"));
+  // we have to send 404 because the server does know the request resource does not found.
+  res.status(404).res.sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(PORT, () => console.log(`server starter on port ${PORT}`));
